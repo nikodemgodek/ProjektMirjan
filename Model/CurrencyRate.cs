4d7884dc2b404258dd45c2b5868f59ba408586f4
Nikodem Godek <nikodem.godek6@gmail.com>
2025-05-05 21:50:22 +0200

    added validation, new tests

--- Model/CurrencyRate.cs
+++ Model/CurrencyRate.cs
@@ -1,10 +1,17 @@
-﻿namespace ProjektMirjan.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjektMirjan.Model
 {
     public class CurrencyRate
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Currency { get; set; }
+        [Required]
+        [MaxLength(3)]
         public string Code { get; set; }
+        [Required]
         public decimal Mid { get; set; }
 
         public int CurrencyRateTableId {  get; set; }
--- Model/CurrencyRateTable.cs
+++ Model/CurrencyRateTable.cs
@@ -1,12 +1,17 @@
 ﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjektMirjan.Model
 {
     public class CurrencyRateTable
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Table {  get; set; }
+        [Required]
         public string No {  get; set; }
+        [Required]
         public DateTime EffectiveDate { get; set; }
         public List<CurrencyRate> CurrencyRates { get; set; }
 
